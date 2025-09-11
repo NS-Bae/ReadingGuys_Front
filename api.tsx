5cc38bb0d52bfa3eb36591ad122cb1853fe33ceb
NS-Bae <qw9711@gmail.com>
2025-09-11 21:48:41 +0900

    jwt 토큰인증용 헤더 인증(인터셉터) 추가

--- api.tsx
+++ api.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import * as Keychain from 'react-native-keychain';
 
 const api = axios.create({
   baseURL: 'http://10.0.2.2:3000',
@@ -6,5 +7,44 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+api.interceptors.request.use(
+  async(config) => {
+    try
+    {
+      const credentials = await Keychain.getGenericPassword();
+
+      if(credentials)
+      {
+        const token = credentials.password;
+
+        if(token && config.headers)
+        {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      }
+      return config;
+    }
+    catch(error)
+    {
+      console.error('token fetch error', error);
+      return config;
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if(error.response?.status === 401)
+    {
+      console.log('Token expired or invalied');
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
